81d0b370c739f6c270898e2cf214e359d046ffa4
malek-cdda <malek@cdda.io>
2023-09-25 10:32:33 +0600

    update some

--- hooks/typeCheck.js
+++ hooks/typeCheck.js
@@ -5,15 +5,10 @@ import {
 import store from "@/redux/store";
 
 export const typeDataCheck = (data) => {
-  const datas = store.getState().checkBoxData.checkBoxData;
-  console.log(data);
-  if (datas.some((item) => item.name === data.name)) {
-    let filterData = datas.filter((item) => item.name !== data.name);
-    console.log(filterData);
-    store.dispatch(removeDouble(filterData));
-    // console.log(filterData);
-  } else {
-    store.dispatch(setCheckboxData(data));
-  }
-  //   store.dispatch(setCheckboxData(data));
+  const checkItemData = store.getState().checkBoxData.checkBoxData;
+  checkItemData.some((item) => item.name === data.name)
+    ? store.dispatch(
+        removeDouble(checkItemData.filter((item) => item.name !== data.name))
+      )
+    : store.dispatch(setCheckboxData(data));
 };
